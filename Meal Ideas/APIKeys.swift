f18129a0464d7d257b83d2b50ec5cea0def99d96
OvershotHavik <plavetzky@gmail.com>
2021-11-19 12:31:19 -0500

    testing .ignore for apices

---
+++ Meal Ideas/APIKeys.swift
@@ -0,0 +1,14 @@
+//
+//  APIKeys.swift
+//  Meal Ideas
+//
+//  Created by Steve Plavetzky on 11/19/21.
+//
+
+import Foundation
+
+final class APIKeys{
+    let test = "1234"
+    let secret = "secret"
+    let test2 = "12345"
+}
