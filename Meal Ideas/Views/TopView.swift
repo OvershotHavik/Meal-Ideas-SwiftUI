f03bfdc26c14aaac2f258d274c87e6e3d19c34bd
OvershotHavik <plavetzky@gmail.com>
2022-01-20 11:02:52 -0500

    Cleaned up Top View, and added a - Selected to the menu text to show what they selected, only shows when the query type was already selected before

--- Meal Ideas/Views/TopView.swift
+++ Meal Ideas/Views/TopView.swift
@@ -7,78 +7,14 @@
 
 import SwiftUI
 
-struct TopView: View {
-    @State private var isActive = false
+struct TopView: View{
+    @EnvironmentObject var query : Query
     @Binding var keywordSearchTapped: Bool
     @Binding var getRandomMeals : Bool
     @Binding var source: Source
-    var body: some View {
-        ZStack{
-//            BackgroundGradientView()
-            KeywordSearchView(keywordSearchTapped: $keywordSearchTapped,
-                              getRandomMeals: $getRandomMeals)
-
-        }
-//                Spacer(minLength: 10)
-//                Text("Meal Ideas")
-//                    .font(.title)
-                
-        
-            
-//                    .frame(height: 150)
-
-                
-//                TopViewButtons(getRandomMeals: $getRandomMeals)
-            
-//        }
-        
-    }
-}
-
-
-struct TopView_Previews: PreviewProvider {
-    static var previews: some View {
-        TopView(keywordSearchTapped: .constant(false), getRandomMeals: .constant(false), source: .constant(.spoonacular))
-    }
-}
-
-
-/*
-// MARK: - Top View Buttons
-struct TopViewButtons: View{
-    @Binding var getRandomMeals: Bool
-
-    var body: some View{
-        HStack{
-            Spacer()
-            RandomQueryButtonView(getRandomMeals: $getRandomMeals)
-            
-            Spacer()
-            
-            CategoryQueryButtonView()
-            
-            Spacer()
-            
-            IngredientQueryButtonView()
-            
-            Spacer()
-        }
-        .padding(.bottom, 5)
-        
-    }
-}
-*/
-
-// MARK: - Keyword Search View
-
-struct KeywordSearchView: View{
-    @EnvironmentObject var query : Query
-    @Binding var keywordSearchTapped : Bool
-    @Binding var getRandomMeals: Bool
     var body: some View{
         HStack(spacing: 20){
             Button(action: {
-                // do random
                 print("Surprise me tapped")
                 query.queryType = .random
                 query.selected = ""
@@ -99,27 +35,32 @@ struct KeywordSearchView: View{
             Menu{
                 Button {
                     query.menuSelection = .category
-//                    query.queryType = .category
                     print("category selected")
                 } label: {
-                    Text(QueryType.category.rawValue)
+                    if query.queryType == .category{
+                        Text("\(QueryType.category.rawValue) - \(query.selected)")
+                    } else {
+                        Text(QueryType.category.rawValue)
+                    }
                 }
                 
                 
                 Button {
                     query.menuSelection = .ingredient
-//                    query.queryType = .ingredient
                     print("Ingredient selected")
                 } label: {
-                    Text(QueryType.ingredient.rawValue)
+                    if query.queryType == .ingredient{
+                        Text("\(QueryType.ingredient.rawValue) - \(query.selected)")
+                    } else {
+                        Text(QueryType.ingredient.rawValue)
+                    }
                 }
-
-                } label: {
+                
+                } label: {// Menu label
                 Image(systemName: "slider.horizontal.3")
             }
                 .font(.title)
                 .foregroundColor(.primary)
-//            Spacer()
         }
         .padding()
     }
