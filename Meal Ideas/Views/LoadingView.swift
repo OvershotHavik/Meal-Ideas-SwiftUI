6c1d11d26f125bebdb080f3c8f52b313982dac01
OvershotHavik <plavetzky@gmail.com>
2022-01-21 14:52:20 -0500

    Added a Stack to the loading view with spacers to make it centered

--- Meal Ideas/Views/LoadingView.swift
+++ Meal Ideas/Views/LoadingView.swift
@@ -10,11 +10,16 @@ import SwiftUI
 struct loadingView: View{
     var body: some View{
         ZStack{
+            VStack{
+                Spacer()
+                ProgressView() // Optional title
+                    .progressViewStyle(CircularProgressViewStyle(tint: .primary))
+                    .scaleEffect(2) // doubles the size
+                Spacer()
+            }
 //            Color(.systemBackground)
 //                .ignoresSafeArea()
-            ProgressView() // Optional title
-                .progressViewStyle(CircularProgressViewStyle(tint: .primary))
-                .scaleEffect(2) // doubles the size
+
         }
     }
 }
