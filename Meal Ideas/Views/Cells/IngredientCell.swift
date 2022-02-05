4894b566d6282cb67b253f258ba55346febb4433
OvershotHavik <plavetzky@gmail.com>
2022-02-05 06:24:54 -0500

    Removed the async image loader reference since I am not using that anymore

--- Meal Ideas/Views/Cells/IngredientCell.swift
+++ Meal Ideas/Views/Cells/IngredientCell.swift
@@ -11,7 +11,6 @@ struct IngredientCell: View {
     var ingredient: Ingredients.Meals
     var selected: Bool
     @State private var image: UIImage?
-    @Environment(\.asyncImageLoader) private var asyncImageLoader
 
     var body: some View {
         let modifiedMealDB = ingredient.strIngredient.replacingOccurrences(of: " ", with: "%20")
@@ -34,65 +33,12 @@ struct IngredientCell: View {
         }
     }
 }
+ 
 /*
 struct IngredientCell_Previews: PreviewProvider {
     static var previews: some View {
         IngredientCell(ingredient: Ingredients.Meals)
     }
 }
+
 */
-
-
-// TODO:  Keep for later integration with async image loader once developer added
-
-
-/*
- struct IngredientCell: View {
-     var ingredient: Ingredients.Meals
-     var selected: Bool
-     @State private var image: UIImage?
-     @Environment(\.asyncImageLoader) private var asyncImageLoader
-
-     var body: some View {
-         let modifiedMealDB = ingredient.strIngredient.replacingOccurrences(of: " ", with: "%20")
-         if let mealDBImages = URL(string: "\(BaseURL.ingredientImage)\(modifiedMealDB).png"){
-             ZStack(alignment: .leading){
- //                Color.blue // only applies to the cell, not the list
-                 
-                 HStack{
-                     if let image = image {
-                         Image(uiImage: image)
-                             .aspectRatio(contentMode: .fit)
-                             .frame(width: 50)
-                     } else {
-                         UI.placeholderImage
-                             .aspectRatio(contentMode: .fit)
-                             .frame(width: 50)
-                     }
-                     
- //                    LoadRemoteImageView(urlString: mealDBImages.absoluteString)
-
-                     Text(ingredient.strIngredient)
-                         .font(.body)
-                         .padding(.horizontal)
-                     Spacer()
-                     if selected == true{
-                         Image(systemName: "checkmark")
-                     }
-                 }
-             }
-             .task{
-                 await loadImage(at: mealDBImages)
-             }
-         }
-     }
-     
-     func loadImage(at source: URL) async {
-         do {
-             image = try await asyncImageLoader.fetch(source)
-         } catch {
-             print(error)
-         }
-     }
- }
- */
